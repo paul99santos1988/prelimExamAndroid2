9c96c21bdbf9c3e5eb97593859c45e1d89c42ae1
Paul Joseph Dar Santos <paul99santos@gmail.com>
2016-12-16 09:59:39 +0800

    First commit

---
+++ app/src/main/java/com/garcestechnology/pauljosephdarsantos/android2prelimexam/MainActivity.java
@@ -0,0 +1,13 @@
+package com.garcestechnology.pauljosephdarsantos.android2prelimexam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
